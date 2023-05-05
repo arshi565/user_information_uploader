17909bde8bccbe0573d7364cf5d51a76f7b28098
arshi565 <72153053+arshi565@users.noreply.github.com>
2023-05-05 22:39:01 +0530

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,5 @@
+// check if any form fields are empty
+if (nameField.value === "" || emailField.value === "" || phoneField.value === "") {
+  // show an alert message
+  alert("Please fill in all the required fields.");
+}
