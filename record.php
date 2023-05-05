7fa7f1e8ce866a2c06796f72eb41dcfb43d5400a
arshi565 <72153053+arshi565@users.noreply.github.com>
2023-05-05 22:38:32 +0530

    Create record.php

---
+++ record.php
@@ -0,0 +1,27 @@
+// connect to the database
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// insert user inputs into the database
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+$sql = "INSERT INTO users (name, email, phone) VALUES ('$name', '$email', '$phone')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
