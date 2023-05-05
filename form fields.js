4b95b91a58d7a06088a3cb31d8f42d48b7da0b5c
arshi565 <72153053+arshi565@users.noreply.github.com>
2023-05-05 22:39:28 +0530

    Create form fields.js

---
+++ form fields.js
@@ -0,0 +1,9 @@
+// get form fields by their ID
+var nameField = document.getElementById("name");
+var emailField = document.getElementById("email");
+var phoneField = document.getElementById("phone");
+
+// set form field values
+nameField.value = name;
+emailField.value = email;
+phoneField.value = phone;
